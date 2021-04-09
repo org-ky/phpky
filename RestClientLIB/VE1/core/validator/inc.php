0ad4663f9e573a56a35cb770f0e6fdb86c6bd462
org-ky <kyko2323@gmail.com>
2021-04-09 12:54:21 +0200

    Create inc.php

---
+++ RestClientLIB/VE1/core/validator/inc.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once ('VE1_Validator.php');
+require_once ('VE1_IValidator.php');
+require_once ('VE1_CodFiscValidator.php');
+require_once ('VE1_NumericValidator.php');
+require_once ('VE1_IntegerValidator.php');
+
+?>
