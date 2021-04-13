aace0f689426c2bdad7c589150e0d36e2cb99d64
org-ky <kyko2323@gmail.com>
2021-04-13 10:09:02 +0200

    Update ArcGetAllNewsBeanIn.php

--- RestClientLIB/VE1/services/arclubservices/beanIn/ArcGetAllNewsBeanIn.php
+++ RestClientLIB/VE1/services/arclubservices/beanIn/ArcGetAllNewsBeanIn.php
@@ -5,7 +5,7 @@
 class ArcGetAllNewsBeanIn implements VE3_IBeanIn
 {
 
-	private $numeroElementiPagina;
+    private $numeroElementiPagina;
     private $offset;
 
     function __construct()
@@ -13,53 +13,61 @@ class ArcGetAllNewsBeanIn implements VE3_IBeanIn
         $this->customData = array();
     }
 
-    function setNumeroElementiPagina($numeroElementiPagina){
-		$this->numeroElementiPagina=$numeroElementiPagina;
+    function setNumeroElementiPagina($numeroElementiPagina)
+    {
+	$this->numeroElementiPagina=$numeroElementiPagina;
+    }
+
+    function getNumeroElementiPagina()
+    {
+	return $this->numeroElementiPagina;
+    }
+
+    function setOffset($offset)
+    {
+	$this->offset=$offset;
+    }
+
+    function getOffset()
+    {
+	return $this->offset;
+    }
+
+    function setCustomData($customData)
+    {
+	$this->customData=$customData;
+    }
+
+    function getCustomData()
+    {
+	return $this->customData;
+    }
+
+    function getDataToSend(){
+
+	$data=new StdClass();
+
+	$data->numeroElementiPagina=$this->numeroElementiPagina;
+	$data->offset=$this->offset;
+
+    	foreach ($this->customData as $key => $value){
+
+		switch ($key){
+			case 'dataDa':
+			case 'dataA':
+			case 'titolo':
+			case 'categoria':
+			case 'tipo':
+			case 'pubblicato':
+				$data->$key=$value;
+				break;
+	        }
+
 	}
 
-	function getNumeroElementiPagina(){
-		return $this->numeroElementiPagina;
-	}
-
-    function setOffset($offset){
-		$this->offset=$offset;
-	}
-
-	function getOffset(){
-		return $this->offset;
-	}
-
-    function setCustomData($customData){
-		$this->customData=$customData;
-	}
-
-	function getCustomData(){
-		return $this->customData;
-	}
-
-  function getDataToSend(){
-		$data=new StdClass();
-
-		$data->numeroElementiPagina=$this->numeroElementiPagina;
-		$data->offset=$this->offset;
-
-    foreach ($this->customData as $key => $value){
-
-			switch ($key){
-        case 'dataDa':
-        case 'dataA':
-        case 'titolo':
-        case 'categoria':
-        case 'tipo':
-        case 'pubblicato':
-					$data->$key=$value;
-					break;
-	    }
-
-		}
-
-		return $data;
-	}
+	return $data;
+	
+    }
 
 }
 ?>
