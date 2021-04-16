a2873191e8e44c5b6c5bd73f3c673bccc5895c1e
org-ky <kyko2323@gmail.com>
2021-04-16 10:50:00 +0200

    Create ArcUpdateLink.php

---
+++ RestClientLIB/VE1/services/arclubservices/ArcUpdateLink.php
@@ -0,0 +1,27 @@
+<?php
+
+class ArcUpdateLink extends VE3_Service implements VE3_IService
+{
+
+    function __construct($connector, $url, $method)
+    {
+        parent::__construct($connector, $url, $method);
+    }
+
+    public function validate($input)
+    {
+		    //TODO verificare i parametri di input qui...
+    	  return $this->runValidation();
+    }
+
+    public function execute($input)
+    {
+        //TODO gestire l'eccezione
+    	$serviceResult=$this->dispatch($input);
+    	$out=$serviceResult;
+
+    	return $out;
+
+    }
+}
+?>
