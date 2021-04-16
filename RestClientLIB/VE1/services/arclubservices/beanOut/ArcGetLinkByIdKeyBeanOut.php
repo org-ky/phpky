f139e4f99bc32ee3eb2126b68322feea8f18a675
org-ky <kyko2323@gmail.com>
2021-04-16 10:22:06 +0200

    Create ArcGetLinkByIdKeyBeanOut.php

---
+++ RestClientLIB/VE1/services/arclubservices/beanOut/ArcGetLinkByIdKeyBeanOut.php
@@ -0,0 +1,17 @@
+<?php
+
+class ArcGetLinkByIdKeyBeanOut implements VE3_IBeanOut
+{
+
+    function __construct()
+    {
+
+    }
+
+	  function getFields()
+    {
+		  return array();
+	  }
+
+}
+?>
