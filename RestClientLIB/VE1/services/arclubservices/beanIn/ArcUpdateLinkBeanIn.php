8432b4f60a5e116dd33ba8be6516292bd88f8cd6
org-ky <kyko2323@gmail.com>
2021-04-16 11:34:36 +0200

    Create ArcUpdateLinkBeanIn.php

---
+++ RestClientLIB/VE1/services/arclubservices/beanIn/ArcUpdateLinkBeanIn.php
@@ -0,0 +1,57 @@
+<?php
+
+class ArcUpdateLinkBeanIn implements VE3_IBeanIn
+{
+
+	  private $id;
+
+    function __construct()
+    {
+        $this->customData = array();
+    }
+
+	  function setId($id)
+    {
+		    $this->id=$id;
+	  }
+
+	  function getId()
+    {
+		    return $this->id;
+	  }
+
+    function setCustomData($customData)
+    {
+		    $this->customData = $customData;
+	  }
+
+	  function getCustomData()
+    {
+		    return $this->customData;
+	  }
+
+    function getDataToSend()
+    {
+		  $data=new StdClass();
+
+		  $data->id=$this->id;
+
+      foreach ($this->customData as $key => $value)
+      {
+
+        switch ($key){
+          case 'chiave':
+          case 'valore':
+                  case 'tipo':
+                  case 'stato':
+            $data->$key=$value;
+            break;
+        }
+
+		  }
+
+		  return $data;
+	}
+
+}
+?>
