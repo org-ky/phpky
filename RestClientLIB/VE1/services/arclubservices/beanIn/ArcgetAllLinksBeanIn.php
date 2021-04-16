dc5e28ea487350422ddd57eb5a58f0b6387ea1f8
org-ky <kyko2323@gmail.com>
2021-04-16 10:02:22 +0200

    Create ArcgetAllLinksBeanIn.php

---
+++ RestClientLIB/VE1/services/arclubservices/beanIn/ArcgetAllLinksBeanIn.php
@@ -0,0 +1,21 @@
+<?php
+
+class ArcGetAllLinksBeanIn implements VE3_IBeanIn
+{
+
+    function __construct()
+    {
+
+    }
+
+
+    function getDataToSend()
+    {
+		  $data=new StdClass();
+
+		  return $data;
+	  }
+
+}
+
+?>
