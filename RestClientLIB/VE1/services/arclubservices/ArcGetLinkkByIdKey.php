adc17b9c8d0d3709d68189ea7477d87f642af4a3
org-ky <kyko2323@gmail.com>
2021-04-16 11:39:12 +0200

    Update ArcGetLinkkByIdKey.php

--- RestClientLIB/VE1/services/arclubservices/ArcGetLinkkByIdKey.php
+++ RestClientLIB/VE1/services/arclubservices/ArcGetLinkkByIdKey.php
@@ -1,19 +1,17 @@
 <?php
 
-//require_once APPPATH . 'core/VE3_Service.php'; //Necessary. If we want CodeIgniter to know how to find this class, we have to modify/extend the core.
-
-class ArcGetLinkByIdKey extends VE3_Service implements VE3_IService
+class ArcGetLinkByIdKey extends VE1_Service implements VE1_IService
 {
 
     public function validate($input)
     {
-		  //TODO verificare i parametri di input qui...
+	//TODO verificare i parametri di input qui...
     	return $this->runValidation();
     }
 
     public function execute($input)
     {
-      //TODO gestire l'eccezione
+      	//TODO gestire l'eccezione
     	$serviceResult=$this->dispatch($input);
     	$out=$serviceResult;
 
