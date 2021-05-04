b825af6ce385d7682e023f7ebc2d5e1b02071544
org-ky <kyko2323@gmail.com>
2021-05-04 12:03:27 +0200

    Update inc.php

--- TestRestClientLIB/inc.php
+++ TestRestClientLIB/inc.php
@@ -1,3 +1,13 @@
 <?php
+  //CORE
   require_once ('../RestClientLIB/VE1/core/Service.php');
+  require_once ('../RestClientLIB/VE1/core/VE1_IService.php');  
+  require_once ('../RestClientLIB/VE1/core/VE1_Service.php');  
+  require_once ('../RestClientLIB/VE1/core/VE1_IRestConnector.php');
+  require_once ('../RestClientLIB/VE1/core/VE1_BusinessProcessManager.php');
+  require_once ('../RestClientLIB/VE1/core/VE1_IBeanIn.php');
+  require_once ('../RestClientLIB/VE1/core/VE1_IBeanOut.php');
+  require_once ('../RestClientLIB/VE1/core/VE1_Singleton.php');
+
+  //CONNECTOR
 ?>
