7220dbe2025ace3f168b80213846402cb2fe490c
org-ky <kyko2323@gmail.com>
2021-05-04 14:50:18 +0200

    Create DYNCRMAPI_GetContattoByCodiceClienteBeanOut.php

---
+++ RestClientLIB/VE1/services/contattidyn/beanOut/DYNCRMAPI_GetContattoByCodiceClienteBeanOut.php
@@ -0,0 +1,16 @@
+<?php
+
+class DYNCRMAPI_GetContattoByCodiceClienteBeanOut implements VE3_IBeanOut
+{
+
+    function __construct()
+    {
+
+    }
+
+	  function getFields(){
+		  return array();
+	  }
+
+}
+?>
