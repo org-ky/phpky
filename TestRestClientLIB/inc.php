7ebb12a16cd7cbabee83a3687f8b84dc0eeed3ae
org-ky <kyko2323@gmail.com>
2021-05-04 15:15:18 +0200

    Update inc.php

--- TestRestClientLIB/inc.php
+++ TestRestClientLIB/inc.php
@@ -1,4 +1,5 @@
 <?php
+
   //CORE
   require_once ('../RestClientLIB/VE1/core/Service.php');
   require_once ('../RestClientLIB/VE1/core/VE1_IService.php');  
@@ -9,5 +10,21 @@
   require_once ('../RestClientLIB/VE1/core/VE1_IBeanOut.php');
   require_once ('../RestClientLIB/VE1/core/VE1_Singleton.php');
 
-  //CONNECTOR
+  //CORE CONNECTORS
+  require_once ('../RestClientLIB/VE1/core/connectors/RestClient.php');
+  require_once ('../RestClientLIB/VE1/core/connectors/VE1_Connector.php');
+  require_once ('../RestClientLIB/VE1/core/connectors/CCSConnector.php');
+  require_once ('../RestClientLIB/VE1/core/connectors/DYNCRMApiConnector.php');
+  require_once ('../RestClientLIB/VE1/core/connectors/MSOnlineConnector.php');
+
+  //SERVICES FACTORY
+  require_once ('../RestClientLIB/VE1/services/AccessTokenServicesFactory.php');
+  require_once ('../RestClientLIB/VE1/services/ContattiDynServicesFactory.php');
+  require_once ('../RestClientLIB/VE1/services/ArcServicesFactory,php');
+
+  //COMMON FACTORY
+  require_once ('../RestClientLIB/VE1/common/MSOnlineApi/MSOnlineApiFactory.php');
+  require_once ('../RestClientLIB/VE1/common/DYNCRMApi/DYNCRMApiFactory.php');
+  require_once ('../RestClientLIB/VE1/common/CCServices/CCServicesFactory.php.php');
+
 ?>
