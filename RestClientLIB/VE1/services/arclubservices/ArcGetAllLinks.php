2278a76f0bef7a0095063e626414b3d1e033d9d4
org-ky <kyko2323@gmail.com>
2021-05-04 11:49:17 +0200

    Update ArcGetAllLinks.php

--- RestClientLIB/VE1/services/arclubservices/ArcGetAllLinks.php
+++ RestClientLIB/VE1/services/arclubservices/ArcGetAllLinks.php
@@ -5,18 +5,18 @@ class ArcGetAllLinks extends VE1_Service implements VE1_IService
 
     public function validate($input){
 
-		  //TODO verificare i parametri di input qui...
+	//TODO verificare i parametri di input qui...
     	return $this->runValidation();
 
     }
 
     public function execute($input)
     {
-      //TODO gestire l'eccezione
+      	//TODO gestire l'eccezione
     	$serviceResult=$this->dispatch($input);
     	$out=$serviceResult;
 
-      return $out;
+      	return $out;
     }
 
 }
