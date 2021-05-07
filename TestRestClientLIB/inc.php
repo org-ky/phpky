b5349deef332e3adf203a3dc5e533e178f4ad08e
org-ky <kyko2323@gmail.com>
2021-05-07 09:42:10 +0200

    Update inc.php

--- TestRestClientLIB/inc.php
+++ TestRestClientLIB/inc.php
@@ -17,14 +17,14 @@
   require_once ('../RestClientLIB/VE1/core/connectors/DYNCRMApiConnector.php');
   require_once ('../RestClientLIB/VE1/core/connectors/MSOnlineConnector.php');
 
-  //SERVICES FACTORY
-  require_once ('../RestClientLIB/VE1/services/AccessTokenServicesFactory.php');
-  require_once ('../RestClientLIB/VE1/services/ContattiDynServicesFactory.php');
-  require_once ('../RestClientLIB/VE1/services/ArcServicesFactory,php');
-
   //COMMON FACTORY
   require_once ('../RestClientLIB/VE1/common/MSOnlineApi/MSOnlineApiFactory.php');
   require_once ('../RestClientLIB/VE1/common/DYNCRMApi/DYNCRMApiFactory.php');
   require_once ('../RestClientLIB/VE1/common/CCServices/CCServicesFactory.php.php');
+
+  //SERVICES FACTORY
+  require_once ('../RestClientLIB/VE1/services/AccessTokenServicesFactory.php');
+  require_once ('../RestClientLIB/VE1/services/ContattiDynServicesFactory.php');
+  require_once ('../RestClientLIB/VE1/services/ArcServicesFactory,php');
 
 ?>
